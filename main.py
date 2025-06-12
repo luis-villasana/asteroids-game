abe140f2c701d89adb8397b18c7799cc63145b86
luis-villasana <luisvillasana020@gmail.com>
2025-06-11 20:56:40 -0500

    feat(main): added code to restrict game to draw a maximum of 60 times per second (60 FPS)

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init    
+    pygame.init()
     screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,9 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
